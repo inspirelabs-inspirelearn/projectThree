e62f007842afb05a149affb6a0eeb55b664e5e41
inspirelabs-inspirelearn <73635821+inspirelabs-inspirelearn@users.noreply.github.com>
2020-11-17 12:30:46 +0200

    Update content.js

--- inspire_updated_02/jquery/content.js
+++ inspire_updated_02/jquery/content.js
@@ -1,5 +1,5 @@
 
-// $(window).load(function() {
+$(document).ready(function() {
 // console.log("content function");
 // //   var ps_audio_intro = document.getElementById("ps_audio_intro");
 // //   var sdcard = navigator.getDeviceStorage("sdcard");
@@ -9,31 +9,41 @@
 // //     ps_audio_intro.src = window.URL.createObjectURL(file);
 // // //     ps_audio_intro.play();
 // // }
-// });
+var sdcard = navigator.getDeviceStorage("sdcard");
+
+request=sdcard.get(".insplearn/first_pic.jpg");
+
+
+
+request.onsuccess = function () {
+  var file = request.result;
+  var first_pic =   document.getElementById("first_pic");
+  first_pic.src = window.URL.createObjectURL(file);
+}
+
+request_Two =sdcard.get(".insplearn/second_pic.jpg");
+request_Two.onsuccess = function () {
+  var file = request_Two.result;
+  var second_pic =   document.getElementById("second_pic");
+  second_pic.src = window.URL.createObjectURL(file);
+}
+
+
+
+request_Three =sdcard.get(".insplearn/third_pic.jpg");
+request_Three.onsuccess = function () {
+  var file = request_Three.result;
+  var third_pic =   document.getElementById("third_pic");
+  third_pic.src = window.URL.createObjectURL(file);
+}
+
+});
 
 // document.getElementById("ps_clickable").load(function(){console.log(content function");});
 // $("#english_focus").load()
-var playing =false;
+var playing =false; //is media playing or not?
+var current_playing= null; //which media is playing?
 
-// function play_parts_of_speech(){
-//   // console.log("Playing parts of speech");
-//   // var ps_audio_intro = document.getElementById("ps_audio_intro");
-//   //  var sdcard = navigator.getDeviceStorage("music");
-//   //  request=sdcard.get("ps_audio_intro.mp3");
-//   //  request.onsuccess = function () {
-//   //    var file = request.result;
-//   //    ps_audio_intro.src = window.URL.createObjectURL(file);
-//      if(playing){
-//        document.getElementById("ps_clickable").innerText="Pause";
-//        playing=false;
-//        ps_audio_intro.pause();
-//      }else{
-//        document.getElementById("ps_clickable").innerText="Play";
-//        playing=true;
-//        ps_audio_intro.play();
-//      }
-//
-//    }
 
    function play_ns_intro(){
      // console.log("Playing parts of speech");
@@ -43,14 +53,27 @@ var playing =false;
       request.onsuccess = function () {
         var file = request.result;
         ns_intro.src = window.URL.createObjectURL(file);
-        if(playing){
-          document.getElementById("ns_intro").innerText="Play";
-          playing=false;
-          ns_intro.pause();
-        }else{
-          document.getElementById("ns_intro").innerText="Pause";
+        if(!playing){
+          ns_intro.play(); //start playing audio
+          playing=true; //signal to say media is playing
+          current_playing = ns_intro; // assing as current playing media
+          document.getElementById("ns_intro_btn").innerText="Stop";
+        }else if(current_playing != null && current_playing == ns_intro){
+          ns_intro.stop();
+          playing = false;
+          current_playing =null;
+          document.getElementById("ns_intro_btn").innerText="Play";
+        }
+
+        else{
+          if(current_playing == null){
+            current_playing.stop();
+          }
+           //stop whatever was playing first
           playing=true;
-          ps_audio_intro.play();
+          ns_intro.play();
+          current_playing = ns_intro;
+          document.getElementById("ns_intro_btn").innerText="Stop";
         }
 
       }
@@ -64,14 +87,25 @@ function play_brain_sections(){
    request.onsuccess = function () {
      var file = request.result;
      brain_sections.src = window.URL.createObjectURL(file);
-     if(playing){
-       document.getElementById("brain_sections").innerText="Play";
-       playing=false;
-       brain_sections.pause();
-     }else{
-       document.getElementById("brain_sections").innerText="Pause";
+     if(!playing){
        playing=true;
        brain_sections.play();
+       current_playing = brain_sections;
+       document.getElementById("brain_sections_btn").innerText="Stop";
+     }
+     else if(current_playing != null && current_playing == brain_sections){
+       playing = false;
+       current_playing = null;
+       brain_sections.stop();
+        document.getElementById("brain_sections").innerText="Play";
+     }else{
+       if(current_playing == null){
+         current_playing.stop();
+       }
+       playing = true;
+       current_playing = brain_sections;
+       brain_sections.play();
+       document.getElementById("brain_sections").innerText="Stop";
      }
 
    }
@@ -86,14 +120,25 @@ function play_the_forebrain(){
    request.onsuccess = function () {
      var file = request.result;
     the_forebrain.src = window.URL.createObjectURL(file);
-     if(playing){
-       document.getElementById("the_forebrain").innerText="Play";
-       playing=false;
-       the_forebrain.pause();
-     }else{
-       document.getElementById("the_forebrain").innerText="Pause";
-       playing=true;
+     if(!playing){
+       playing =true;
+       document.getElementById("the_forebrain_btn").innerText="Stop";
        the_forebrain.play();
+       current_playing = the_forebrain;
+     }else if (current_playing !=  null && current_playing == the_forebrain){
+       playing=false;
+       current_playing =null;
+       the_forebrain.stop();
+       document.getElementById("the_forebrain_btn").innerText="Play";
+     }else{
+       if(current_playing == null){
+         current_playing.stop();
+       }
+       current_playing = the_forebrain;
+       playing =true;
+       the_forebrain.play();
+       document.getElementById("the_forebrain_btn").innerText="Stop";
+
      }
 
    }
@@ -107,14 +152,25 @@ function play_the_cerebrum(){
    request.onsuccess = function () {
      var file = request.result;
     the_cerebrum.src = window.URL.createObjectURL(file);
-     if(playing){
-       document.getElementById("the_cerebrum").innerText="Play";
-       playing=false;
-       the_cerebrum.pause();
-     }else{
-       document.getElementById("the_cerebrum").innerText="Pause";
+     if(!playing){
+       document.getElementById("the_cerebrum_btn").innerText="Stop";
        playing=true;
        the_cerebrum.play();
+       current_playing = the_cerebrum;
+     }else if(current_playing != null && current_playing == the_cerebrum){
+       the_cerebrum.stop();
+       current_playing =  null;
+       playing = false;
+        document.getElementById("the_cerebrum_btn").innerText="Play";
+     }else{
+
+       if(current_playing == null){
+         current_playing.stop();
+       }
+       document.getElementById("the_cerebrum_btn").innerText="Stop";
+       playing=true;
+       the_cerebrum.play();
+       current_playing = the_cerebrum;
      }
 
    }
@@ -130,14 +186,24 @@ function play_the_midbrain(){
   request.onsuccess = function () {
     var file = request.result;
    the_midbrain.src = window.URL.createObjectURL(file);
-    if(playing){
-      document.getElementById("the_midbrain").innerText="Play";
-      playing=false;
-      the_midbrain.pause();
-    }else{
-      document.getElementById("the_midbrain").innerText="Pause";
+    if(!playing){
+      document.getElementById("the_midbrain_btn").innerText="Stop";
       playing=true;
       the_midbrain.play();
+      current_playing = the_midbrain;
+    }else if (current_playing != null && current_playing == the_midbrain){
+      the_midbrain.stop();
+      current_playing = null;
+      playing = false;
+      document.getElementById("the_midbrain_btn").innerText="Play";
+    }else{
+      if(current_playing == null){
+        current_playing.stop();
+      }
+      document.getElementById("the_midbrain_btn").innerText="Stop";
+      playing=true;
+      the_midbrain.play();
+      current_playing = the_midbrain;
     }
 
   }
@@ -152,14 +218,24 @@ function play_the_hindbrain(){
   request.onsuccess = function () {
     var file = request.result;
    the_hindbrain.src = window.URL.createObjectURL(file);
-    if(playing){
-      document.getElementById("the_hindbrain").innerText="Play";
-      playing=false;
-      the_hindbrain.pause();
-    }else{
-      document.getElementById("the_hindbrain").innerText="Pause";
+    if(!playing){
+      document.getElementById("the_hindbrain_btn").innerText="Stop";
       playing=true;
       the_hindbrain.play();
+      current_playing = the_hindbrain;
+    }else if(current_playing != null && current_playing == the_hindbrain){
+      the_hindbrain.stop();
+      playing = false;
+      current_playing = null;
+      document.getElementById("the_hindbrain_btn").innerText="Play";
+    }else{
+      if(current_playing == null){
+        current_playing.stop();
+      }
+      document.getElementById("the_hindbrain_btn").innerText="Stop";
+      playing=true;
+      the_hindbrain.play();
+      current_playing = the_hindbrain;
     }
 
   }
