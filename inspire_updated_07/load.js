a3cfb6e5c77080d801a2b4a3ca3597b918e14a18
inspirelabs-inspirelearn <73635821+inspirelabs-inspirelearn@users.noreply.github.com>
2020-12-01 16:53:50 +0200

    Update load.js

--- inspire_updated_07/load.js
+++ inspire_updated_07/load.js
@@ -9,27 +9,38 @@ function loadHomeScreen(){
 
             //window.location.assign("pages/home.html");
 }
-// function gotoWisdom(){
-//     window.location.assign("wisdom.html");
-// }
-// function gotoStudy(){
-//     window.location.assign("study.html");
-// }
-// function gotoHome(){
-//     window.location.assign("home.html");
-// }
 
-// function carousel() {
-//     var i;
-//     var x = document.getElementsByClassName("mySlides");
-//     for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//     }
-//     myIndex++;
-//     if (myIndex > x.length) {myIndex = 1}
-//     x[myIndex-1].style.display = "block";
-//     setTimeout(carousel, 3500);
-// }
+document.getElementById("btnToLink").addEventListener("click", initiateDownloads);
+
+function initiateDownloads() {
+  var mediaContent = '{'+
+  '"contentObject" : ['+
+    '{"mediaFile":"mediaContent/audioTwo.mp3"},'+
+    '{"mediaFile":"mediaContent/videoOne.mp4"}'+
+  ']'+
+'}';
+
+var obj = JSON.parse(mediaContent);
+
+var creatLink = document.getElementById("theDownloadLink");
+document.getElementById("firstDisplay").style="display: none";
+document.getElementById("secondDisplay").style="display: block";
+  
+
+var theprogressvalue = 0;
+var ObjectLength = obj.contentObject.length;
+theprogressvalue = 100 / ObjectLength;
+  
+  for (i = 0; i < ObjectLength; i++) {
+    // creatLink = obj.contentObject[i].mediaFile;
+    creatLink.download = obj.contentObject[i].mediaFile;
+    creatLink.href= obj.contentObject[i].mediaFile;
+    creatLink.click();
+    document.getElementById("theProgress").style="width: "+theprogressvalue+"%";
+
+  }
+  console.log("hello there!!! it is finally working");
+}
 
 
 
